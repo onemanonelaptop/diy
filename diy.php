7144f351b1103c2c5b60a429499c90520d7b4367
Rob Holmes <rob@onemanonelaptop.com>
2012-01-29 10:15:16 +0000

    Updated GPL license

--- diy.php
+++ diy.php
@@ -8,7 +8,22 @@
  * Author URI: http://github.com/onemanonelaptop
  */
 
-/*  Copyright 2011 Rob Holmes ( email: rob@onemanonelaptop.com ) */
+/*  Copyright 2011 Rob Holmes ( email: rob@onemanonelaptop.com )
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 // Ensure the class is available to all dependant plugins by using plugins_loaded
 add_action( 'plugins_loaded', 'diy_init' );
